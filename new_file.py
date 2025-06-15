358d915da5b5d9f33f2fd2ce43d0febcdede7907
Christopher Adzima <cjadzima@wpi.edu>
2025-06-14 22:03:50 -0400

    Create new file with function.

---
+++ new_file.py
@@ -0,0 +1,13 @@
+# This is a new file to be uploaded to the repository.
+
+def format_string(s):
+    """
+    Formats the input string by capitalizing the first letter and adding a period at the end.
+    
+    Args:
+        s (str): The input string to format.
+        
+    Returns:
+        str: The formatted string.
+    """
+    return s.capitalize() + '.'
